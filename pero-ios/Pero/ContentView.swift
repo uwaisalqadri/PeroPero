32dc14f398c4f83f953fd91be2d6b2d938fd3ccb
Uwais Alqadri <uwaisalqadri654321@gmail.com>
2024-04-28 19:27:11 +0800

    chore: init both android and ios project

---
+++ pero-ios/Pero/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  PeroPero
+//
+//  Created by Uwais Alqadri on 28/4/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+  var body: some View {
+    VStack {
+      Image(systemName: "globe")
+        .imageScale(.large)
+      Text("Hello, world!")
+    }
+    .padding()
+  }
+}
+
+#Preview {
+  ContentView()
+}
---
+++ pero-ios/Pero/MainApp.swift
@@ -0,0 +1,17 @@
+//
+//  MainApp.swift
+//  PeroPero
+//
+//  Created by Uwais Alqadri on 28/4/24.
+//
+
+import SwiftUI
+
+@main
+struct MainApp: App {
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+    }
+  }
+}
